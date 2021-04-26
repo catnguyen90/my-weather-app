28adcc3911ba39761303b7d891f4c38f84aa665d
catnguyen90 <catnguyen90@gmail.com>
2021-04-25 21:42:26 -0400

    JS- API integration, HTML- inserted celsius/fahrenheit links

---
+++ src/index.js
@@ -0,0 +1,15 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#temp-description");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
+
+let units = "imperial";
+let apiKey = "841f4173fd81916cf9b0019ffbaf6e87";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(displayTemperature);
