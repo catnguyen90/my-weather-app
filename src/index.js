410d240524aca44dfad0006d566fa4d62828c565
catnguyen90 <catnguyen90@gmail.com>
2021-05-03 19:28:11 -0400

    store response from api inside forecast variable, looping through the forecast variable, including current icon, running min and max temp, displayed only the first 6 indexes

--- src/index.js
+++ src/index.js
@@ -22,26 +22,44 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
           <div class="col-2">
-            <div class="weather-forecast-date">
-            ${day}</div> 
-            <i class="fas fa-sun"></i><br />
+            <div class="weather-forecast-date">${formatDay(forecastDay.dt)}
+            </div> 
+            
+            <img
+            src="http://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png"
+            alt=""
+            width="42"/><br />
             <div class="weather-forecast-temperature">
               <span class="weather-forecast-max">
-            66°</span>
-            <span class="weather-forecast-min">  49°</span>
+            ${Math.round(forecastDay.temp.max)}°</span>
+            <span class="weather-forecast-min">${Math.round(
+              forecastDay.temp.min
+            )}°</span>
           </div>
         </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
