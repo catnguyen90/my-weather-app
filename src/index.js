2b710202432c4cb1561f853de9d7962431a24611
catnguyen90 <catnguyen90@gmail.com>
2021-05-02 21:55:08 -0400

    removed forecast html and placed into js

--- src/index.js
+++ src/index.js
@@ -22,6 +22,31 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+          <div class="col-2">
+            <div class="weather-forecast-date">
+            ${day}</div> 
+            <i class="fas fa-sun"></i><br />
+            <div class="weather-forecast-temperature">
+              <span class="weather-forecast-max">
+            66°</span>
+            <span class="weather-forecast-min">  49°</span>
+          </div>
+        </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -89,3 +114,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+
+displayForecast();
